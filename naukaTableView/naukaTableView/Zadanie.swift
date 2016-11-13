f43778745e249f3307f6e164c28799b228f16d8d
Paweł Liczmański <paweliczek@gmail.com>
2016-11-13 22:07:32 +0100

    day 1

---
+++ naukaTableView/naukaTableView/Zadanie.swift
@@ -0,0 +1,13 @@
+//
+//  zadanie.swift
+//  naukaTableView
+//
+//  Created by Paweł Liczmański on 13.11.2016.
+//  Copyright © 2016 Paweł Liczmański. All rights reserved.
+//
+
+import Foundation
+class Zadanie: NSObject {
+    var text = ""
+    var checked = true
+}
