479e52ec2a7320f457b7bada4e2c50553fbcc442
Paweł Liczmański <paweliczek@gmail.com>
2016-11-20 21:25:21 +0100

    04 done ale trzeba koniecznie jeszcze raz to zrobić

---
+++ naukaTableView/naukaTableView/ChecklistItem.swift
@@ -0,0 +1,9 @@
+//
+//  ChecklistItem.swift
+//  naukaTableView
+//
+//  Created by Paweł Liczmański on 18.11.2016.
+//  Copyright © 2016 Paweł Liczmański. All rights reserved.
+//
+
+import Foundation
---
+++ naukaTableView/naukaTableView/ChecklistViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ChecklistViewController.swift
+//  naukaTableView
+//
+//  Created by Paweł Liczmański on 18.11.2016.
+//  Copyright © 2016 Paweł Liczmański. All rights reserved.
+//
+
+import Foundation
