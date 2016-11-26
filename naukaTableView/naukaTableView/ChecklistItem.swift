f6e2ad83dd783bb17884b5ffc3a847b3129ebe29
Paweł Liczmański <paweliczek@gmail.com>
2016-11-26 22:21:08 +0100

    05 finished not bad

---
+++ naukaTableView/naukaTableView/ChecklistItem.swift
@@ -0,0 +1,18 @@
+//
+//  ChecklistItem.swift
+//  naukaTableView
+//
+//  Created by Paweł Liczmański on 26.11.2016.
+//  Copyright © 2016 Paweł Liczmański. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem: NSObject {
+    var text = ""
+    var checked = true
+    
+    func toggleCheckmark() {
+        checked = !checked
+    }
+}
